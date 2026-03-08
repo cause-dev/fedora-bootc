6c43b9011942efab132ad1e0bcd11acd709381d4
cause-dev <cause.dev@proton.me>
2026-03-08 15:42:12 +0500

    fix hostname not working after deploying the image

--- config.sh
+++ config.sh
@@ -2,7 +2,9 @@
 set -ouex pipefail
 
 # Set Hostname
-echo "dev" > /etc/hostname
+mkdir -p /usr/lib/tmpfiles.d
+echo 'L /etc/hostname - - - - ../usr/lib/hostname' > /usr/lib/tmpfiles.d/hostname.conf
+echo "dev" > /usr/lib/hostname
 
 # Set Default Shell (Fish)
 sed -i 's/SHELL=\/bin\/bash/SHELL=\/usr\/bin\/fish/' /etc/default/useradd
